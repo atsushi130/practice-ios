4a9573776d752f244036f8ac8fbec50a518ddec9
atsushi130 <atsushi_main@outlook.jp>
2017-12-28 19:50:14 +0900

    add item structure

--- PracticeApp/ItemList/Model/Item.swift
+++ PracticeApp/ItemList/Model/Item.swift
@@ -2,8 +2,10 @@
 //  Item.swift
 //  PracticeApp
 //
-//  Created by 三宅　篤史 on 2017/12/28.
+//  Created by Atsushi Miyake on 2017/12/28.
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+struct Item {
+    let name: String
+}
