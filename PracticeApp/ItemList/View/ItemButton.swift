0590a39dd387149d11a32cb28457427a48a0fc5d
atsushi130 <atsushi_main@outlook.jp>
2017-12-28 19:50:40 +0900

    designed item cell

--- PracticeApp/ItemList/View/ItemButton.swift
+++ PracticeApp/ItemList/View/ItemButton.swift
@@ -2,8 +2,47 @@
 //  ItemButton.swift
 //  PracticeApp
 //
-//  Created by 三宅　篤史 on 2017/12/28.
+//  Created by Atsushi Miyake on 2017/12/28.
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ItemButton: UIView {
+    
+    @IBOutlet private weak var imageView:  UIImageView!
+    @IBOutlet private weak var countLabel: UILabel!
+    @IBOutlet private weak var typeLabel:  UILabel!
+    
+    enum ButtonType: String {
+        case wants = "wants"
+        case haves = "haves"
+    }
+    
+    var buttonType: ButtonType = .wants {
+        didSet {
+            self.changeImage()
+            self.changeTypeLabel()
+        }
+    }
+    
+    private var isOn = false {
+        didSet { self.changeImage() }
+    }
+    
+    private var suffix: String {
+        get { return self.isOn ? "_on" : "_off" }
+    }
+    
+    private func changeImage() {
+        self.imageView.image = UIImage(named: self.buttonType.rawValue + self.suffix)
+    }
+    
+    private func changeTypeLabel() {
+        self.typeLabel.text = self.buttonType.rawValue
+    }
+    
+    @IBAction func tap() {
+        self.isOn = !self.isOn
+    }
+}
