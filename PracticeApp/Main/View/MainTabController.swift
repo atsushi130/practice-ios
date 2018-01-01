996a94be75e43bb41403ed77a031a7ddb088faed
atsushi130 <atsushi_main@outlook.jp>
2018-01-02 00:53:32 +0900

    update images

---
+++ PracticeApp/Main/View/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/01.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
