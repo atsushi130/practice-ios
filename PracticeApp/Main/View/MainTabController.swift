342ad290a494a00512fc3b56b5a5005e70ca23dc
atsushi130 <atsushi_main@outlook.jp>
2018-01-02 00:57:16 +0900

    implemented tab bar and navigation bar

--- PracticeApp/Main/View/MainTabController.swift
+++ PracticeApp/Main/View/MainTabController.swift
@@ -7,10 +7,29 @@
 //
 
 import UIKit
+import UIFontComplete
 
-final class MainViewController: UITabBarController {
+final class MainTabController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setup()
+    }
+    
+    private func setup() {
+        self.setupNavigationBar()
+        self.setupTabBar()
+    }
+    
+    private func setupNavigationBar() {
+        guard let font = UIFont(font: .hiraginoSansW6, size: 18.5) else { return }
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: font]
+    }
+    
+    private func setupTabBar() {
+        UITabBar.appearance().tintColor               = UIColor.theme
+        UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+        guard let font = UIFont(font: .hiraginoSansW6, size: 10) else { return }
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: font, NSAttributedStringKey.foregroundColor: UIColor.theme], for: .selected)
     }
 }
