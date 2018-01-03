af727816f1c07d970dfda38898329a4eb93152d7
atsushi130 <atsushi_main@outlook.jp>
2018-01-04 00:00:00 +0900

    add ItemLogView

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemLogView.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemLogView.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ItemLogView: UIView {
+    @IBOutlet private weak var userIconView: UserIconView!
+    @IBOutlet private weak var logLabel: UILabel!
+}
