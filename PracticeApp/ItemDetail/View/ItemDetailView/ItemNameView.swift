765d23ffc622a03aa217df0dd3bcb125beb4f114
atsushi130 <atsushi_main@outlook.jp>
2018-01-04 00:00:17 +0900

    add ItemNameView

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemNameView.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemNameView.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ItemNameView: UIView {
+    
+    @IBOutlet private weak var mainNameLabel: UILabel!
+    @IBOutlet private weak var subNameLabel:  UILabel!
+    
+    func bind(name: (main: String, sub: String)) {
+        self.mainNameLabel.text = name.main
+        self.subNameLabel.text  = name.sub
+    }
+}
