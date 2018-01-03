6f837606d496963e88b0e9df66674b440e47c8b3
atsushi130 <atsushi_main@outlook.jp>
2018-01-04 00:01:27 +0900

    designed ItemDetailReusableView

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView.swift
@@ -10,13 +10,19 @@ import UIKit
 
 final class ItemDetailReusableView: UICollectionReusableView {
     
+    @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var reactionBarsView: ReactionBarsView!
     @IBOutlet private weak var itemNameView: ItemNameView!
     @IBOutlet private weak var itemInformationView: ItemInformationView!
     @IBOutlet private weak var itemLogView: ItemLogView!
     @IBOutlet private weak var itemActionView: ItemActionView!
+    @IBOutlet private weak var reactionFooterView: ReactionFooterView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func bind(itemDetail: ItemDetail) {
+        self.itemNameView.bind(name: (main: itemDetail.name, sub: itemDetail.subName))
     }
 }
