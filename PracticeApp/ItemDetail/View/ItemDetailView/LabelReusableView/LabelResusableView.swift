63f8861a84ab3f737ea1a5df1a650878888e548c
atsushi130 <atsushi_main@outlook.jp>
2018-01-04 18:04:47 +0900

    refactor directory

---
+++ PracticeApp/ItemDetail/View/ItemDetailView/LabelReusableView/LabelResusableView.swift
@@ -0,0 +1,9 @@
+//
+//  LabelResusableView.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/04.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
