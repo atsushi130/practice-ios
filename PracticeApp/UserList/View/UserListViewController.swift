402d80dcab5830be2337d44a0bb0cac56edf0b80
atsushi130 <atsushi_main@outlook.jp>
2018-01-05 02:22:09 +0900

    add UserListViewController

--- PracticeApp/UserList/View/UserListViewController.swift
+++ PracticeApp/UserList/View/UserListViewController.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class UserListViewController: UIViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        didSet {
+            self.layout = UICollectionViewFlowLayout()
+            self.collectionView.collectionViewLayout = self.layout
+            self.collectionView.ex.register(cellType: UserCell.self)
+        }
+    }
+    
+    private var layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout() {
+        didSet {
+            self.layout.itemSize = CGSize(width: self.collectionView.frame.size.width, height: 120)
+            self.layout.minimumLineSpacing = 0
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension UserListViewController: UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
+
+extension UserListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return self.collectionView.ex.dequeueReusableCell(with: UserCell.self, for: indexPath)
+    }
+}
