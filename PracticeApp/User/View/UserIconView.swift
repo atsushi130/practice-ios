ae890280d1de914a9bb1fa28634e5eebce255dcc
atsushi130 <atsushi_main@outlook.jp>
2018-01-04 22:51:22 +0900

    UserIconView change directory

--- PracticeApp/User/View/UserIconView.swift
+++ PracticeApp/User/View/UserIconView.swift
@@ -10,6 +10,14 @@ import UIKit
 
 @IBDesignable final class UserIconView: UIView {
     
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    @IBInspectable
+    var image: UIImage? {
+        get { return self.imageView.image }
+        set { self.imageView.image = newValue }
+    }
+    
     @IBInspectable
     var cornerRadius: CGFloat {
         get { return self.layer.cornerRadius }
