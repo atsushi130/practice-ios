74fb4ba25e0d61ed5984da26817a779935792ccc
atsushi130 <atsushi_main@outlook.jp>
2018-01-05 02:17:00 +0900

    add UserListViewModel

---
+++ PracticeApp/UserList/View/UserListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UserListViewController.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/05.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
---
+++ PracticeApp/UserList/ViewModel/UserListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserListViewModel.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/05.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+struct UserListViewModel {
+    
+}
