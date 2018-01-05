39dc0b31604d262a5f9f7aed77caf9b89b93881b
atsushi130 <atsushi_main@outlook.jp>
2018-01-05 21:56:26 +0900

    add new files

---
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailFoldersCell/ItemDetailFolderCell.swift
@@ -0,0 +1,9 @@
+//
+//  ItemDetailFolderCell.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/05.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
---
+++ PracticeApp/ItemList/View/ItemLayout/ItemLayout.swift
@@ -0,0 +1,9 @@
+//
+//  ItemLayout.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/05.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
