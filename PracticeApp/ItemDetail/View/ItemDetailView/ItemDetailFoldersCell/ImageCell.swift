5191c841df228f5f9241be1f704c4dccb7691387
atsushi130 <atsushi_main@outlook.jp>
2018-01-05 21:58:01 +0900

    add ImageCell

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailFoldersCell/ImageCell.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailFoldersCell/ImageCell.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.imageView.image = #imageLiteral(resourceName: "sample")
+    }
+}
