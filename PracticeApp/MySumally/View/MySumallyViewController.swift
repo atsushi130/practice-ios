963c90733ed2d7b4b8afd3e26066eb7753dc9c63
atsushi130 <atsushi_main@outlook.jp>
2018-01-06 04:34:45 +0900

    work in progress MySumallyViewController

--- PracticeApp/MySumally/View/MySumallyViewController.swift
+++ PracticeApp/MySumally/View/MySumallyViewController.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class MySumallyViewController: UIViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        didSet {
+            self.layout = UICollectionViewFlowLayout()
+            self.collectionView.collectionViewLayout = self.layout
+            self.collectionView.ex.register(cellType: ItemCollectionCell.self)
+        }
+    }
+    
+    private var layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout() {
+        didSet {
+            self.layout.estimatedItemSize = CGSize(width: 1.0, height: 1.0)
+            self.layout.sectionInset      = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension MySumallyViewController: UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension MySumallyViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.ex.dequeueReusableCell(with: ItemCollectionCell.self, for: indexPath)
+        
+        let margin = self.layout.sectionInset.left + self.layout.sectionInset.right
+        cell.collectionViewWidth = self.collectionView.frame.size.width - margin - (cell.collectionViewMargin * 2)
+        cell.matrix = ItemCollectionCell.Matrix(x: 5, y: 3)
+        
+        return cell
+    }
+}
