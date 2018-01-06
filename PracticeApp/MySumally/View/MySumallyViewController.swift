0a1cbf5c193f291d3edb0469b21d62a38ccdb9ee
atsushi130 <atsushi_main@outlook.jp>
2018-01-06 23:28:28 +0900

    update

--- PracticeApp/MySumally/View/MySumallyViewController.swift
+++ PracticeApp/MySumally/View/MySumallyViewController.swift
@@ -20,10 +20,14 @@ final class MySumallyViewController: UIViewController {
     
     private var layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout() {
         didSet {
-            self.layout.estimatedItemSize = CGSize(width: 1.0, height: 1.0)
-            self.layout.sectionInset      = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+            self.layout.estimatedItemSize  = CGSize(width: 1.0, height: 1.0)
+            self.layout.sectionInset       = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+            self.layout.minimumInteritemSpacing = 10.0
+            self.layout.minimumLineSpacing      = 15.0
         }
     }
+    
+    private let cellTypes: [ItemCollectionCell.CellType] = [.wants, .haves, .adds, .folder, .folder]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +45,7 @@ extension MySumallyViewController: UICollectionViewDelegate {
 extension MySumallyViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -49,7 +53,19 @@ extension MySumallyViewController: UICollectionViewDataSource {
         
         let margin = self.layout.sectionInset.left + self.layout.sectionInset.right
         cell.collectionViewWidth = self.collectionView.frame.size.width - margin - (cell.collectionViewMargin * 2)
-        cell.matrix = ItemCollectionCell.Matrix(x: 5, y: 3)
+        
+        let cellType = self.cellTypes[indexPath.row]
+        switch cellType {
+        case .folder:
+            cell.collectionViewWidth = (self.collectionView.frame.size.width - self.layout.minimumInteritemSpacing - margin).ex.half.ex.floor - (cell.collectionViewMargin * 2)
+            cell.matrix = ItemCollectionCell.Matrix(x: 3, y: 3)
+            
+        default:
+            cell.collectionViewWidth = self.collectionView.frame.size.width - margin - (cell.collectionViewMargin * 2)
+            cell.matrix = ItemCollectionCell.Matrix(x: 5, y: 3)
+        }
+        
+        cell.cellType = self.cellTypes[indexPath.row]
         
         return cell
     }
