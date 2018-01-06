84497cd83a5374f4b22234574fe58f99f1469151
atsushi130 <atsushi_main@outlook.jp>
2018-01-07 01:38:20 +0900

    refactoring

---
+++ PracticeApp/MySumally/View/NewFolderCell/NewFolderCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewFolderCell.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/01/07.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+final class NewFolderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
