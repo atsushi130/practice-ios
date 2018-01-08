df4b073106e4c415b587271a3da0d664b826feb5
atsushi130 <atsushi_main@outlook.jp>
2018-01-08 23:28:29 +0900

    add FunCell

--- PracticeApp/UserList/View/UserProfileReusableView/FunListView/FunCell.swift
+++ PracticeApp/UserList/View/UserProfileReusableView/FunListView/FunCell.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class FunCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var brandLabel: UILabel!
+    
+    var text: String = "" {
+        didSet {
+            self.brandLabel.text = self.text
+            self.setNeedsLayout()
+            self.brandLabel.sizeToFit()
+            self.layoutIfNeeded()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.brandLabel.preferredMaxLayoutWidth = 345.0
+        self.brandLabel.layer.cornerRadius      = self.brandLabel.frame.size.height.ex.half
+    }
+}
