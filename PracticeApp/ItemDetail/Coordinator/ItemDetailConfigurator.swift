9c122e4dbacd7054dc1c74c121f860b919c82252
atsushi130 <atsushi_main@outlook.jp>
2018-09-09 22:12:41 +0900

    add: ItemDetailConfigurator

---
+++ PracticeApp/ItemDetail/Coordinator/ItemDetailConfigurator.swift
@@ -0,0 +1,9 @@
+//
+//  ItemDetailConfigurator.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/09/09.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
