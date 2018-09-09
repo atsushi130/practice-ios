f33c44e327fed292cdff46ad27fb5a31fbedd28c
atsushi130 <atsushi_main@outlook.jp>
2018-09-09 22:47:46 +0900

    add: MySumallyConfigurator

---
+++ PracticeApp/MySumally/Coordinator/MySumallyConfigurator.swift
@@ -0,0 +1,15 @@
+//
+//  MySumallyConfigurator.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/09/09.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+import CoordinatorKit
+
+struct MySumallyConfigurator: Configurator {
+    typealias VC = MySumallyViewController
+    static func configure(with vc: MySumallyConfigurator.VC, dependency: MySumallyConfigurator.VC.Dependency) {}
+}
