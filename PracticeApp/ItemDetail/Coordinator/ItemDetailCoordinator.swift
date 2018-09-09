f3296a7b4217135c0774049ec76f1fd6f672fafc
atsushi130 <atsushi_main@outlook.jp>
2018-09-10 01:17:59 +0900

    implement: ItemDetailCoordinator

--- PracticeApp/ItemDetail/Coordinator/ItemDetailCoordinator.swift
+++ PracticeApp/ItemDetail/Coordinator/ItemDetailCoordinator.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import CoordinatorKit
+
+struct ItemDetailCoordinator: Coordinator {
+    
+    weak var viewController: UIViewController?
+    
+    enum Route {
+        case userList(reactionType: ReactionView.ReactionType)
+        case detail
+        
+        var viewController: UIViewController {
+            switch self {
+            case .userList: return UserListViewController.instantiate()
+            case .detail:   return ItemDetailViewController.instantiate()
+            }
+        }
+        
+        var navigationTitle: String {
+            switch self {
+            case let .userList(reactionType): return reactionType.title
+            case .detail: return ""
+            }
+        }
+    }
+    
+    func transition(to route: Route) {
+        let viewController = route.viewController
+        self.viewController?.navigationController?.pushViewController(viewController, animated: true)
+        viewController.navigationItem.title = route.navigationTitle
+    }
+}
