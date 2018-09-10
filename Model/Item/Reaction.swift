f13da87ba25c6283d21e00f6a3620f1453dd1d22
atsushi130 <atsushi_main@outlook.jp>
2018-09-11 01:16:06 +0900

    implement: Reaction structure

---
+++ Model/Item/Reaction.swift
@@ -0,0 +1,36 @@
+//
+//  Reaction.swift
+//  Model
+//
+//  Created by Atsushi Miyake on 2018/09/11.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public struct Reaction: Decodable {
+    
+    public let wants: (state: Bool, count: Int)
+    public let haves: (state: Bool, count: Int)
+    
+    private enum CodingKeys: String, CodingKey {
+        case wants
+        case haves
+    }
+    
+    private enum NestedKeys: String, CodingKey {
+        case state
+        case count
+    }
+    
+    public init(from decoder: Decoder) throws {
+        
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let wants = try values.nestedContainer(keyedBy: NestedKeys.self, forKey: .wants)
+        self.wants = (state: try wants.decode(Bool.self, forKey: .state), count: try wants.decode(Int.self, forKey: .count))
+        
+        let haves = try values.nestedContainer(keyedBy: NestedKeys.self, forKey: .haves)
+        self.haves = (state: try haves.decode(Bool.self, forKey: .state), count: try haves.decode(Int.self, forKey: .count))
+    }
+}
