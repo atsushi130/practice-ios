2689f5cce891e12f43f2b76a67d7e7e9e822e3ea
atsushi130 <atsushi_main@outlook.jp>
2018-09-11 01:16:18 +0900

    implement: Item structure

---
+++ Model/Item/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Model
+//
+//  Created by Atsushi Miyake on 2018/09/11.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public struct Item: Decodable {
+    public let id: String
+    public let name: String
+    public let subName: String
+    public let reaction: Reaction
+}
