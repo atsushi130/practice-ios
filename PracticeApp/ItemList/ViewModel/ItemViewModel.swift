0d62c9c325b43e849ca28e524b37f38f8f2ef37d
atsushi130 <atsushi_main@outlook.jp>
2018-09-11 01:17:28 +0900

    implement: using ApiClient

--- PracticeApp/ItemList/ViewModel/ItemViewModel.swift
+++ PracticeApp/ItemList/ViewModel/ItemViewModel.swift
@@ -10,6 +10,8 @@ import RxSwift
 import RxCocoa
 import Connectable
 import NSObject_Rx
+import Model
+import Data
 
 final class ItemViewModel: Connectable {
     
@@ -26,40 +28,17 @@ final class ItemViewModel: Connectable {
         
         self.coordinator = coordinator
         
-        let items = [
-            Item(id: "1", name: "Main Item1", subName: "Sub Name1", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "2", name: "Main Item2", subName: "Sub Name2", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "3", name: "Main Item3", subName: "Sub Name3", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "54", name: "Main Item4", subName: "Sub Name4", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "6", name: "Main Item5", subName: "Sub Name5", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "7", name: "Main Item6", subName: "Sub Name6", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "8", name: "Main Item7", subName: "Sub Name7", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "9", name: "Main Item8", subName: "Sub Name8", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "10", name: "Main Item9", subName: "Sub Name9", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "11", name: "Main Item10", subName: "Sub Name10", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "12", name: "Main Item11", subName: "Sub Name11", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "13", name: "Main Item12", subName: "Sub Name12", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "14", name: "Main Item13", subName: "Sub Name13", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "15", name: "Main Item14", subName: "Sub Name14", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "16", name: "Main Item15", subName: "Sub Name15", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "17", name: "Main Item16", subName: "Sub Name16", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "18", name: "Main Item17", subName: "Sub Name17", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "19", name: "Main Item18", subName: "Sub Name18", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "20", name: "Main Item19", subName: "Sub Name19", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "21", name: "Main Item20", subName: "Sub Name20", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "22", name: "Main Item21", subName: "Sub Name21", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "23", name: "Main Item22", subName: "Sub Name22", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "24", name: "Main Item23", subName: "Sub Name23", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "25", name: "Main Item24", subName: "Sub Name24", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "26", name: "Main Item25", subName: "Sub Name25", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "27", name: "Main Item26", subName: "Sub Name26", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "28", name: "Main Item27", subName: "Sub Name27", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "29", name: "Main Item28", subName: "Sub Name28", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "30", name: "Main Item29", subName: "Sub Name29", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0)),
-            Item(id: "31", name: "Main Item30", subName: "Sub Name30", isOn: (wants: true, haves: false), count: (wants: 0, haves: 0))
-        ]
-        
-        self.items.accept(items)
+        ApiClient.ItemService.shared.fetchAll()
+            .map { items in
+                return items.map { item in
+                    let isOn  = (wants: item.reaction.wants.state, haves: item.reaction.haves.state)
+                    let count = (wants: item.reaction.wants.count, haves: item.reaction.haves.count)
+                    return Item(id: item.id, name: item.name, subName: item.subName, isOn: isOn, count: count)
+                }
+            }
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.items)
+            .disposed(by: self.disposeBag)
     }
 }
 
