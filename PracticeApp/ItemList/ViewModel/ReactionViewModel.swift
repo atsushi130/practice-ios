845a0e29129bf895ae8302738ab9af50421c8d99
atsushi130 <atsushi_main@outlook.jp>
2018-09-12 00:29:35 +0900

    fix

--- PracticeApp/ItemList/ViewModel/ReactionViewModel.swift
+++ PracticeApp/ItemList/ViewModel/ReactionViewModel.swift
@@ -11,24 +11,24 @@ enum ReactionViewModel {
     case wants
     case haves
 
-    func changeState(reaction: Reaction) -> Reaction {
+    func changeState(to reactionState: ReactionState) -> ReactionState {
         switch self {
-        case .wants: return self.changeWantsState(reaction: reaction)
-        case .haves: return self.changeHavesState(reaction: reaction)
+        case .wants: return self.changeWantsState(reactionState)
+        case .haves: return self.changeHavesState(reactionState)
         }
     }
     
-    private func changeWantsState(reaction: Reaction) -> Reaction {
-        switch reaction.haves {
-        case true:  return Reaction(wants: true, haves: false)
-        case false: return Reaction(wants: !reaction.wants, haves: reaction.haves)
+    private func changeWantsState(_ reactionState: ReactionState) -> ReactionState {
+        switch reactionState.haves {
+        case true:  return ReactionState(wants: true, haves: false)
+        case false: return ReactionState(wants: !reactionState.wants, haves: reactionState.haves)
         }
     }
     
-    private func changeHavesState(reaction: Reaction) -> Reaction {
-        switch reaction.wants {
-        case true:  return Reaction(wants: false, haves: true)
-        case false: return Reaction(wants: reaction.wants, haves: !reaction.haves)
+    private func changeHavesState(_ reactionState: ReactionState) -> ReactionState {
+        switch reactionState.wants {
+        case true:  return ReactionState(wants: false, haves: true)
+        case false: return ReactionState(wants: reactionState.wants, haves: !reactionState.haves)
         }
     }
 }
