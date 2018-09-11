7cabd6f0124d0ed5017874fcad84cabc3214befc
atsushi130 <atsushi_main@outlook.jp>
2018-09-11 17:09:37 +0900

    update: change to class from struct

--- PracticeApp/ItemList/Model/Item.swift
+++ PracticeApp/ItemList/Model/Item.swift
@@ -6,10 +6,18 @@
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
-struct Item: Itemable {
+final class Item: Itemable {
     let id: String
     let name:    String
     let subName: String
     var isOn: (wants: Bool, haves: Bool)
     var count: (wants: Int, haves: Int)
+    
+    init(id: String, name: String, subName: String, isOn: (wants: Bool, haves: Bool), count: (wants: Int, haves: Int)) {
+        self.id      = id
+        self.name    = name
+        self.subName = subName
+        self.isOn    = isOn
+        self.count   = count
+    }
 }
