9f0757c0d0ed59419cf84046c054672b5912476c
atsushi130 <atsushi_main@outlook.jp>
2018-09-13 00:59:38 +0900

    add: new nib files

---
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemInformationView/ItemInformationBrandView.swift
@@ -0,0 +1,9 @@
+//
+//  ItemInformationBrandView.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/09/13.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
---
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemInformationView/ItemInformationTextView.swift
@@ -0,0 +1,9 @@
+//
+//  ItemInformationTextView.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2018/09/13.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
