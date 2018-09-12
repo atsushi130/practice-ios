d9f4e31926508edee6297d9a6bc807b661d66d2d
atsushi130 <atsushi_main@outlook.jp>
2018-09-13 01:00:14 +0900

    implement: minimum custom ItemInformationBrandView

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemInformationView/ItemInformationBrandView.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemInformationView/ItemInformationBrandView.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftExtensions
+
+@IBDesignable
+final class ItemInformationBrandView: UIView, NibDesignable {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configureNib()
+    }
+}
