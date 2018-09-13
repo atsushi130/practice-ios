5a3d604c3e1a27ec4744072cd46d51706e59e886
atsushi130 <atsushi_main@outlook.jp>
2018-09-13 20:00:22 +0900

    implement: custom ItemActionView

--- PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemActionView/ItemActionView.swift
+++ PracticeApp/ItemDetail/View/ItemDetailView/ItemDetailReusableView/ItemActionView/ItemActionView.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import SwiftExtensions
 
-final class ItemActionView: UIView {
+@IBDesignable
+final class ItemActionView: UIView, NibDesignable {
+    
     @IBOutlet private weak var google: UIButton!
     @IBOutlet private weak var folder: UIButton!
     @IBOutlet private weak var sell:   UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configureNib()
+    }
 }
 
