6a43eb954cda64171cc4bccbb7e7768f3f0906a8
atsushi130 <atsushi_main@outlook.jp>
2018-09-14 18:35:49 +0900

    add: ViewCompoment target

---
+++ ViewComponent/ViewComponent.h
@@ -0,0 +1,19 @@
+//
+//  ViewComponent.h
+//  ViewComponent
+//
+//  Created by Atsushi Miyake on 2018/09/14.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewComponent.
+FOUNDATION_EXPORT double ViewComponentVersionNumber;
+
+//! Project version string for ViewComponent.
+FOUNDATION_EXPORT const unsigned char ViewComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewComponent/PublicHeader.h>
+
+
