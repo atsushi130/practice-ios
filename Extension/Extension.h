f6a177fe87968d40611d6472fb016413dd180e28
atsushi130 <atsushi_main@outlook.jp>
2018-09-15 16:16:05 +0900

    add: Extension module

---
+++ Extension/Extension.h
@@ -0,0 +1,19 @@
+//
+//  Extension.h
+//  Extension
+//
+//  Created by Atsushi Miyake on 2018/09/15.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extension.
+FOUNDATION_EXPORT double ExtensionVersionNumber;
+
+//! Project version string for Extension.
+FOUNDATION_EXPORT const unsigned char ExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extension/PublicHeader.h>
+
+
