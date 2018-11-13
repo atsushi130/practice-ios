327a5c4da40ff3dcc3e2709361df96f1bfbf6f35
atsushi130 <atsushi_main@outlook.jp>
2018-11-14 00:07:48 +0900

    implement: asDriverWithErrorAlert method

--- Extension/Observable+.swift
+++ Extension/Observable+.swift
@@ -6,9 +6,10 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
+import SwiftExtensions
 
 public extension Observable {
     
@@ -22,5 +23,13 @@ public extension Observable {
     
     public func asDriverIgonringError() -> Driver<Element> {
         return self.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    public func asDriverWithErrorAlert(title: String = "", message: String = "", on viewController: UIViewController) -> Driver<Element> {
+        return self.asDriver(onErrorRecover: { [weak viewController] _ in
+            let alert = UIAlertController.okAlertController(title: title, message: message)
+            viewController?.present(alert, animated: true, completion: nil)
+            return .empty()
+        })
     }
 }
