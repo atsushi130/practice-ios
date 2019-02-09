6f46dd22af511e88e13843292abda5d564cc584f
atsushi130 <atsushi_main@outlook.jp>
2019-02-10 00:14:19 +0900

    implement: mainTabController

--- PracticeApp/UniversalLinkCoordinator/PracticeUniversalLinkCoordinator+.swift
+++ PracticeApp/UniversalLinkCoordinator/PracticeUniversalLinkCoordinator+.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2019年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension PracticeUniversalLinkCoordinator {
+    var mainTabController: MainTabController? {
+        if let mainTabController = UIApplication.shared.keyWindow?.rootViewController as? MainTabController {
+            return mainTabController
+        }
+        if let mainTabController = UIApplication.shared.keyWindow?.rootViewController?.tabBarController as? MainTabController {
+            return mainTabController
+        }
+        return nil
+    }
+}
