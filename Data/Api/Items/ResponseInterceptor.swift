d56ceb142fd42bf958ba638c7c0841d30bfc37cc
atsushi130 <atsushi_main@outlook.jp>
2019-02-09 17:43:52 +0900

    implement: SuccessResponseInterceptor

--- Data/Api/Items/ResponseInterceptor.swift
+++ Data/Api/Items/ResponseInterceptor.swift
@@ -14,3 +14,22 @@ import Extension
 protocol ResponseInterceptor {
     func intercept<T>(response: Response) -> Observable<T>? where T: Decodable
 }
+
+final class SuccessResponseInterceptor: ResponseInterceptor {
+    
+    static let shared = SuccessResponseInterceptor()
+    private init() {}
+    
+    func intercept<T>(response: Response) -> Observable<T>? where T : Decodable {
+        switch response.statusCode {
+        case 200...226:
+            do {
+                let decoded = try response.map(T.self, using: .snakeCaseDecoder)
+                return .just(decoded)
+            } catch {
+                return .error(PracticeError.jsonDecodeError(error: error))
+            }
+        default: return nil
+        }
+    }
+}
