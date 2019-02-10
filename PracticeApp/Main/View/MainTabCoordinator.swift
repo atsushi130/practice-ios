39bf4ae292988a3c8facf4c1b1e0d5045bc0c9b2
atsushi130 <atsushi_main@outlook.jp>
2019-02-10 23:24:34 +0900

    implement: MainTabCoordinator

--- PracticeApp/Main/View/MainTabCoordinator.swift
+++ PracticeApp/Main/View/MainTabCoordinator.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import CoordinatorKit
+
+struct MainTabCoordinator: Coordinator {
+    
+    weak var viewController: UIViewController?
+    private var mainTabController: MainTabController? {
+        return self.viewController as? MainTabController
+    }
+    
+    enum Route {
+        case itemList
+        case mySumally
+    }
+    
+    func transition(to route: Route) {
+        switch route {
+        case .itemList:
+            self.mainTabController?.selectedIndex = 0
+        case .mySumally:
+            self.mainTabController?.selectedIndex = 1
+        }
+    }
+}
