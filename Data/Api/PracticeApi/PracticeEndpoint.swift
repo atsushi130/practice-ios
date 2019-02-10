bdd99b3c39b5c37d3c63774c5478e50052bc0397
atsushi130 <atsushi_main@outlook.jp>
2019-02-10 14:40:57 +0900

    define: PracticeEndpoint

--- Data/Api/PracticeApi/PracticeEndpoint.swift
+++ Data/Api/PracticeApi/PracticeEndpoint.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import Moya
+
+protocol PracticeEndpoint: TargetType {}
+
+extension PracticeEndpoint {
+    
+    var baseURL: URL {
+        return URL(string: "localhost:8080")!
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Cookie": "",
+            "User-Agent": "practice-ios/\(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)"
+        ]
+    }
+}
