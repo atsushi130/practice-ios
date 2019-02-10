33b73dc073d5946c936030915ca595b03508946c
atsushi130 <atsushi_main@outlook.jp>
2019-02-10 14:12:15 +0900

    define: ForceTransitionRoute

--- Data/Api/PracticeApi/ForceTransitionRoute.swift
+++ Data/Api/PracticeApi/ForceTransitionRoute.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import Moya
+
+public enum ForceTransitionRoute {
+    
+    case logout
+    case updateRequired
+    case ban
+    
+    init?(statusCode: Int, error: RequestError) {
+        switch statusCode {
+        case 401:
+            self = .logout
+        case 403:
+            guard let code = error.code else { return nil }
+            switch code {
+            case 1: self = .logout
+            case 2: self = .updateRequired
+            case 3: self = .ban
+            default: return nil
+            }
+        default:
+            return nil
+        }
+    }
+}
