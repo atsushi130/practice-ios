232a9d4af8670e2686e29f75942ea9819588000a
atsushi130 <atsushi_main@outlook.jp>
2019-02-10 23:07:11 +0900

    add: MainTabConfigurator file

---
+++ PracticeApp/Main/View/MainTabConfigurator.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabConfigurator.swift
+//  PracticeApp
+//
+//  Created by Atsushi Miyake on 2019/02/10.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
